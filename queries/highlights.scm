c878cede8800f5757edbd260be5f0dbb09448310
teddyrandby <tedrandby@gmail.com>
2022-09-28 17:27:57 +0000

    Fix namespace call with postfix

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,20 +1,5 @@
 ; Highlights.scm for gab
 
-(identifier) @variable
-
-
-(object key: (identifier) @variable.other.member)
-(property property: (identifier) @variable.other.member)
-
-(call receiver: (identifier) @function)
-(call receiver: (property property: (identifier) @function ))
-(method method: (call receiver: (property receiver: (identifier) @type)))
-
-(method method: (identifier) @function)
-(function_definition name: (identifier) @function)
-(object_definition name: (identifier) @type)
-(list_definition name: (identifier) @type)
-
 (comment) @comment
 
 (number) @constant.numeric
@@ -89,8 +74,21 @@
   "."
   ":"
 ] @operator
+(identifier) @variable
+
+
+(object key: (identifier) @variable.other.member)
+(property property: (identifier) @variable.other.member)
+
+(call receiver: (identifier) @function)
+(call receiver: (property property: (identifier) @function ))
+(method method: (call receiver: (property receiver: (identifier) @type)))
+(method method: (post (call receiver: (property receiver: (identifier) @type))))
+
+(method method: (identifier) @function)
+(function_definition name: (identifier) @function)
+(object_definition name: (identifier) @type)
+(list_definition name: (identifier) @type)
 
 (global bang: ("!") @variable.builtin)
 (global name: (identifier) @variable.builtin)
-
-
