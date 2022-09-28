91116236e5ef370e61a21f7457fa4b1e8d8e5f08
teddyrandby <tedrandby@gmail.com>
2022-09-28 17:19:59 +0000

    Adjust highlight priorites

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -7,8 +7,8 @@
 (property property: (identifier) @variable.other.member)
 
 (call receiver: (identifier) @function)
-(method method: (call receiver: (property receiver: (identifier) @type)))
 (call receiver: (property property: (identifier) @function ))
+(method method: (call receiver: (property receiver: (identifier) @type)))
 
 (method method: (identifier) @function)
 (function_definition name: (identifier) @function)
