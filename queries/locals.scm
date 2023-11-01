2a9a045e83485fa3277f76209eabb741589a4d22
teddyrandby <tedrandby@gmail.com>
2023-11-01 03:53:15 +0000

    Change names in local.scm

--- queries/locals.scm
+++ queries/locals.scm
@@ -2,13 +2,13 @@
   (body)
   (function_definition)
   (block)
-] @scope
+] @local.scope
 
-(const_definition name: (identifier) @definition)
-(function_definition name: (identifier) @definition)
-(object_definition name: (identifier) @definition)
-(assignment left: (identifier) @definition)
-(parameters (identifier) @definition)
-(for local: (identifier) @definition)
+(const_definition name: (identifier) @local.definition)
+(function_definition name: (identifier) @local.definition)
+(object_definition name: (identifier) @local.definition)
+(assignment left: (identifier) @local.definition)
+(parameters (identifier) @local.definition)
+(for local: (identifier) @local.definition)
 
-(identifier) @reference
+(identifier) @local.reference
