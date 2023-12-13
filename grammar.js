0464da633585ddb8a6c3260d3a56c60282f5b69f
Teddy Randby <tedrandby@gmail.com>
2023-12-13 21:18:34 +0000

    Rec Item

--- grammar.js
+++ grammar.js
@@ -57,9 +57,9 @@ module.exports = grammar({
             $.symbol,
             $.string,
             seq(
-              '|',
+              '[',
               $._expression,
-              '|'
+              ']'
             ),
           ),
         ),
