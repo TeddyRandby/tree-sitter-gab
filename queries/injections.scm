5a821a204ca4b0c666803a2fa49a33cf3dfb64d8
Teddy Randby <tedrandby@gmail.com>
2024-01-09 15:43:25 +0000

    More injection fix

--- queries/injections.scm
+++ queries/injections.scm
@@ -10,6 +10,18 @@
   (#set! injection.combined)
   (#offset! @injection.content 0 1 0 -1))
 
+((strcall
+  callee: (identifier) @injection.language
+  argument: (string [
+    (doublestring)
+    (singlestring)
+    (interpbegin)
+    (interpmiddle)
+    (interpend)
+  ] @injection.content) .)
+  (#set! injection.combined)
+  (#offset! @injection.content 0 1 0 -1))
+
 ((call
   callee: (identifier) @injection.language
   argument: (string [
