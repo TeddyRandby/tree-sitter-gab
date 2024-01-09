ab63404e3e145a6b30eea6bb8bad692892d490f7
Teddy Randby <tedrandby@gmail.com>
2024-01-09 16:08:49 +0000

    Fix injections

--- queries/injections.scm
+++ queries/injections.scm
@@ -3,6 +3,12 @@
   argument: (string [
     (doublestring)
     (singlestring)
+  ] @injection.content) .)
+  (#offset! @injection.content 0 1 0 -1))
+
+((send
+  message: (message (identifier) @injection.language)
+  argument: (string [
     (interpbegin)
     (interpmiddle)
     (interpend)
@@ -15,6 +21,20 @@
   argument: (string [
     (doublestring)
     (singlestring)
+  ] @injection.content) .)
+  (#offset! @injection.content 0 1 0 -1))
+
+((call
+  callee: (identifier) @injection.language
+  argument: (string [
+    (doublestring)
+    (singlestring)
+  ] @injection.content) .)
+  (#offset! @injection.content 0 1 0 -1))
+
+((strcall
+  callee: (identifier) @injection.language
+  argument: (string [
     (interpbegin)
     (interpmiddle)
     (interpend)
@@ -25,8 +45,6 @@
 ((call
   callee: (identifier) @injection.language
   argument: (string [
-    (doublestring)
-    (singlestring)
     (interpbegin)
     (interpmiddle)
     (interpend)
